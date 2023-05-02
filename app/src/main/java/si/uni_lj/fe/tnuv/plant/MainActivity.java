1b32f04ae30b0ea9a36c988baa945bfdeb179711
Ana Jenko <aj7106@student.uni-lj.si>
2023-05-02 12:25:40 +0200

    navMenu dela

--- app/src/main/java/si/uni_lj/fe/tnuv/plant/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/plant/MainActivity.java
@@ -1,14 +1,49 @@
 package si.uni_lj.fe.tnuv.plant;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import si.uni_lj.fe.tnuv.plant.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new SPOKFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.SPOK:
+                    replaceFragment(new SPOKFragment());
+                    break;
+                case R.id.urnik:
+                    replaceFragment(new UrnikFragment());
+                    break;
+                case R.id.razpisi:
+                    replaceFragment(new RazpisiFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
