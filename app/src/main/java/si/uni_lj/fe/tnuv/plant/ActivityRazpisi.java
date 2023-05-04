808b370022892cfca02173a12e83b828216b2e54
Ana Jenko <aj7106@student.uni-lj.si>
2023-05-04 15:35:11 +0200

    iskanje po ListViewDela (prenovljen ActivityRazpisi.java)

--- app/src/main/java/si/uni_lj/fe/tnuv/plant/ActivityRazpisi.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/plant/ActivityRazpisi.java
@@ -1,25 +1,49 @@
 package si.uni_lj.fe.tnuv.plant;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.SearchView;
 
-public class ActivityRazpisi extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ActivityRazpisi extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    ListView list;
+    ListViewAdapter adapter;
+    SearchView editsearch;
+    String[] razpiski;
+    ArrayList<Razpis> arraylist = new ArrayList<Razpis>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        int layoutId = getIntent().getIntExtra("layoutId", 0);
-
         setContentView(R.layout.activity_razpisi);
-        populateListView();
+
+        razpiski = new String[]{"Razpis 1: Kupovanje novih panjev", "Razpis 2: Pomoč za žrtve poplave", "Razpis 3: Gnojila",
+                "Razpis 4: Predsednik kmetijske zadruge", "Razpis 5: Finančni dodatek kmetu", "Razpis 6: Podarjena koza", "Razpis 7: Brezplačno seme češnjevcev", "Razpis 8: Prehranski dodatki za kokoši",
+                "Razpis 9: Avtomatska vrata za kokošnjak","Razpis 10: Samovozeča kosilnica","Razpis 11: Tečaj 'candlinga'"};
+
+        // Locate the ListView in listview_main.xml
+        list = (ListView) findViewById(R.id.nabor_razpisov);
+
+        for (int i = 0; i < razpiski.length; i++) {
+            Razpis animalNames = new Razpis(razpiski[i]);
+            // Binds all strings into an array
+            arraylist.add(animalNames);
+        }
+
+        // Pass results to ListViewAdapter Class
+        adapter = new ListViewAdapter(this, arraylist);
+
+        // Binds the Adapter to the ListView
+        list.setAdapter(adapter);
+
+        // Locate the EditText in listview_main.xml
+        editsearch = (SearchView) findViewById(R.id.search);
+        editsearch.setOnQueryTextListener(this);
     }
 
     public void startActivitySPOK(View v) {
@@ -37,16 +61,14 @@ public class ActivityRazpisi extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void populateListView() {
-        String[] razpisi = getResources().getStringArray(R.array.reg_nabor_razpisov);
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_item, razpisi);
-
-        ListView listView = findViewById(R.id.nabor_razpisov);
-        listView.setAdapter(adapter);
-
-        AdapterView.OnItemClickListener mListener = (parent, view, position, id) ->
-                Toast.makeText(getApplicationContext(), razpisi[position], Toast.LENGTH_SHORT).show();
-        listView.setOnItemClickListener(mListener);
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        adapter.filter(newText);
+        return false;
     }
 
 }
